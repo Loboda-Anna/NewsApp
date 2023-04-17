2c8edb5b6adaa9d99379e290a1fd3f30285dfdd6
Anna Loboda <107415190+Loboda-Anna@users.noreply.github.com>
2023-04-17 10:42:05 +0300

    Delete src directory

--- src/js/news-text-hiding.js
+++
@@ -1,8 +0,0 @@
-export function textHiding(text, textLength) {
-  if (text.length > textLength) {
-    let newText = text.trim().substring(0, textLength);
-    let lastSpace = newText.lastIndexOf(' ');
-    return newText.substring(0, lastSpace) + '...';
-  }
-  return text;
-}
